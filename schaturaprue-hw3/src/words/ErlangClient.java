3e1d3e5ff95e5f09b16ad753a423eaaa71305794
Tum Chaturapruek <schaturaprue@mallow.cs.hmc.edu>
2014-02-26 02:17:39 -0800

    Update Java Erlang client

--- schaturaprue-hw3/src/words/ErlangClient.java
+++ schaturaprue-hw3/src/words/ErlangClient.java
@@ -19,9 +19,9 @@ import java.rmi.server.UnicastRemoteObject;
 
 import com.ericsson.otp.erlang.OtpAuthException;
 import com.ericsson.otp.erlang.OtpConnection;
+import com.ericsson.otp.erlang.OtpErlangAtom;
 import com.ericsson.otp.erlang.OtpErlangExit;
 import com.ericsson.otp.erlang.OtpErlangList;
-import com.ericsson.otp.erlang.OtpErlangLong;
 import com.ericsson.otp.erlang.OtpErlangObject;
 import com.ericsson.otp.erlang.OtpErlangRef;
 import com.ericsson.otp.erlang.OtpErlangString;
@@ -29,7 +29,8 @@ import com.ericsson.otp.erlang.OtpErlangTuple;
 import com.ericsson.otp.erlang.OtpPeer;
 import com.ericsson.otp.erlang.OtpSelf;
 
-public final class ErlangClient implements RemoteSpelling{
+public final class ErlangClient
+{
   /**
    * The minimum number of command line arguments.
    */
@@ -42,14 +43,13 @@ public final class ErlangClient implements RemoteSpelling{
 
   
   /**
-   * The minimum acceptable port number.
+   * The atoms in the protocol.
    */
-  private static final int MIN_PORT_NUMBER = 1025;
-
-  /**
-   * The maximum port number.
-   */
-  public static final int MAX_PORT_NUMBER = 65535;
+  private static final OtpErlangAtom check = new OtpErlangAtom("check");
+  private static final OtpErlangAtom add = new OtpErlangAtom("add");
+  private static final OtpErlangAtom remove = new OtpErlangAtom("remove");
+  private static final OtpErlangAtom correct = new OtpErlangAtom("correct");
+  private static final OtpErlangAtom incorrect = new OtpErlangAtom("incorrect");
 
   /**
    * the time stamp format.
@@ -103,44 +103,76 @@ public final class ErlangClient implements RemoteSpelling{
       }
 
       final String node_name = the_args[0];
+      final String process_name = "spelling";
       final OtpSelf me = new OtpSelf("schaturaprue" + System.currentTimeMillis());
       final OtpConnection connection = me.connect(new OtpPeer(node_name));
-
-      System.out.print("Sending message to " + the_args[0] + " at " + the_args[1] + ": ");
-      for (int i = 2; i < the_args.length; i++)
-      {
-        System.out.print(the_args[i] + " ");
-      }
-      System.out.println();
       
-      final OtpErlangObject[] message_strings = new OtpErlangObject[the_args.length - 2];
-      for (int i = 2; i < the_args.length; i++)
+      for (int i = 1; i < the_args.length; i++)
       {
-        message_strings[i-2] = new OtpErlangString(the_args[i]);
-      }
-      final OtpErlangList message = new OtpErlangList(message_strings);
-      final OtpErlangRef ref = me.createRef();
-      final OtpErlangTuple tuple =
-        new OtpErlangTuple(new OtpErlangObject[] { me.pid(), ref, message });
-      connection.send(the_args[0], tuple);
-      final OtpErlangObject reply = connection.receive(TIMEOUT);
+        final OtpErlangString message = new OtpErlangString(the_args[i]);
+        ErlangClient.println("Querying server (" + message + ")");
+        final OtpErlangRef ref = me.createRef();
+        final OtpErlangTuple tuple =
+          new OtpErlangTuple(new OtpErlangObject[] { me.pid(), ref, check, message });
+        connection.send(process_name, tuple);
+        final OtpErlangObject reply = connection.receive(TIMEOUT);
 
-      if (reply instanceof OtpErlangTuple &&
-          ((OtpErlangTuple) reply).arity() == 3)
-      {
-        try
+        if (reply instanceof OtpErlangTuple &&
+            ((OtpErlangTuple) reply).arity() == 3)
         {
-          final OtpErlangTuple reply_tuple = (OtpErlangTuple) reply;
-          final OtpErlangRef reply_ref = (OtpErlangRef) reply_tuple.elementAt(0);
-          final OtpErlangLong reply_long = (OtpErlangLong) reply_tuple.elementAt(1);
-          final OtpErlangObject reply_obj = reply_tuple.elementAt(2);
-          if (ref.equals(reply_ref)) {
-            System.out.println("Reply " + reply_long + " from server: " + reply_obj);
+          try
+          {
+            final OtpErlangTuple reply_tuple = (OtpErlangTuple) reply;
+            final OtpErlangRef reply_ref = (OtpErlangRef) reply_tuple.elementAt(0);
+            final OtpErlangAtom reply_result = (OtpErlangAtom) reply_tuple.elementAt(1);
+            final OtpErlangList reply_suggestions = (OtpErlangList) reply_tuple.elementAt(2);
+            if (ref.equals(reply_ref) && reply_result.equals(incorrect))
+            {
+              ErlangClient.println(message + " is spelled incorrectly. Suggestions: " + reply_suggestions);
+            }
+            else
+            {
+              throw new ClassCastException();
+            }
+          }
+          catch (final ClassCastException e)
+          {
+            ErlangClient.println_err("Got unexpected message: " + reply);
           }
         }
-        catch (final ClassCastException e)
+        else if (reply instanceof OtpErlangTuple &&
+            ((OtpErlangTuple) reply).arity() == 2)
         {
-          System.out.println("Got unexpected message: " + reply);
+          try
+          {
+            final OtpErlangTuple reply_tuple = (OtpErlangTuple) reply;
+            final OtpErlangRef reply_ref = (OtpErlangRef) reply_tuple.elementAt(0);
+            final OtpErlangAtom reply_result = (OtpErlangAtom) reply_tuple.elementAt(1);
+            if (ref.equals(reply_ref) && reply_result.equals(correct))
+            {
+              ErlangClient.println(message + " is spelled correctly.");
+            }
+            else if (ref.equals(reply_ref) && reply_result.equals(add))
+            {
+              ErlangClient.println(message + " added.");
+            }
+            else if (ref.equals(reply_ref) && reply_result.equals(remove))
+            {
+              ErlangClient.println(message + " removed.");
+            }
+            else
+            {
+              throw new ClassCastException();
+            }
+          }
+          catch (final ClassCastException e)
+          {
+            ErlangClient.println_err("Got unexpected message: " + reply);
+          }
+        }
+        else
+        {
+          ErlangClient.println_err("Got unexpected message: " + reply);
         }
       }
     }
@@ -164,66 +196,6 @@ public final class ErlangClient implements RemoteSpelling{
       ErlangClient.println_err("An error occurred.");
       e1.printStackTrace();
       System.exit(1);
-    }
-  }
-
-  @Override
-  public List<String> check(String the_word) throws RemoteException {
-    ErlangClient.println("Received query (" + the_word + ")");
-    if (word_list.correct(the_word))
-    {
-      // the word is spelled correctly.
-      ErlangClient.println(the_word + " is spelled correctly.");
-      return null;
-    }
-    else
-    {
-      // the word is spelled incorrectly.
-      List<String> suggestions = word_list.suggestions(the_word);
-      final int num_suggestions = suggestions.size();
-      String suggestion_context;
-      if (num_suggestions == 1)
-      {
-        suggestion_context = " There is " + num_suggestions +
-            " suggested word:";
-      }
-      else
-      {
-        suggestion_context = " There are " + num_suggestions +
-            " suggested words:";
-      }
-      ErlangClient.print(the_word +" is spelled incorrectly." +
-        suggestion_context);
-      for (String suggestion : suggestions)
-      {
-        System.out.print(" " + suggestion);
-      }
-      System.out.println(".");
-      return suggestions;
-    }
-  }
-
-  @Override
-  public void add(String the_word) throws RemoteException {
-    if (word_list.add(the_word))
-    {
-      ErlangClient.println(the_word + " added to word list.");
-    }
-    else
-    {
-      ErlangClient.println(the_word + " already in word list, not added.");
-    }
-  }
-
-  @Override
-  public void remove(String the_word) throws RemoteException {
-    if (word_list.remove(the_word))
-    {
-      ErlangClient.println(the_word + " from word list.");
-    }
-    else
-    {
-      ErlangClient.println(the_word + " not in word list, not removed.");
     }
   }
 }
